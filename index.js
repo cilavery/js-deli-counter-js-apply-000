942c2f24456ea7e870db6fa72818d72d8e671d32
Cecilia Avery <cilsong@gmail.com>
2018-01-11 14:37:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  //return name's position in line
+}
+
+function nowServing() {
+  //return first person in line then remove that person from line
+  //if line empty, return "There is nobody waiting to be served!"
+}
+
+function currentLine() {
+  //returns currentLine with 'The line is currently: 1. Ada, 2. Grace'
+  //If the line is empty, return "The line is currently empty"
+}

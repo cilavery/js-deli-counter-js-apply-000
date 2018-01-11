fb57c2f61e3c62ffbb0bcfd020743ef733a921a5
Cecilia Avery <cilsong@gmail.com>
2018-01-11 15:19:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 
-function takeANumber(name) {
+function takeANumber(katzDeliLine, name) {
   //return name's position in line
   //"Welcome, Ada. You are number 1 in line."
   katzDeliLine.push(name);
